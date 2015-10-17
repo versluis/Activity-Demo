ec7e368be991adece6f47e710a3bbf44ca7f61ea
Jay Versluis <jay@versluis.com>
2015-10-17 15:02:04 -0400

    Initial Commit

---
+++ Activity Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Activity Demo
+//
+//  Created by Jay Versluis on 17/10/2015.
+//  Copyright © 2015 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Activity DemoTests/Activity_DemoTests.m
@@ -0,0 +1,39 @@
+//
+//  Activity_DemoTests.m
+//  Activity DemoTests
+//
+//  Created by Jay Versluis on 17/10/2015.
+//  Copyright © 2015 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Activity_DemoTests : XCTestCase
+
+@end
+
+@implementation Activity_DemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

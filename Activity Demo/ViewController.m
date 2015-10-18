5d292a6bd0ac1becacacdb53912670a1dd93669f
Jay Versluis <jay@versluis.com>
2015-10-17 23:14:40 -0400

    added Popover for iPad presentation

--- Activity Demo/ViewController.m
+++ Activity Demo/ViewController.m
@@ -9,14 +9,18 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) IBOutlet UIStackView *stackView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // hide left bar button item if we're on an iPhone
+    if ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPhone) {
+        self.navigationItem.leftBarButtonItem = nil;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,7 +28,15 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)actionButton:(id)sender {
+- (void)createButtons {
+    
+    // image sharing button
+    
+}
+
+# pragma mark - Sharing Methods
+
+- (IBAction)shareImageDefault:(id)sender {
     
     // grab an item we want to share
     UIImage *image = [UIImage imageNamed:@"three"];
@@ -38,5 +50,65 @@
         // executes after the user selects something
     }];
 }
+
+- (IBAction)shareImageExcludeSocial:(id)sender {
+    
+    // grab an item we want to share
+    UIImage *image = [UIImage imageNamed:@"three"];
+    NSArray *items = @[image];
+    
+    // build an activity view controller
+    UIActivityViewController *controller = [[UIActivityViewController alloc]initWithActivityItems:items applicationActivities:nil];
+    
+    // exclude several items (for example, Facepuke and Shitter)
+    NSArray *excluded = @[UIActivityTypePostToFacebook, UIActivityTypePostToTwitter];
+    controller.excludedActivityTypes = excluded;
+    
+    // and present it
+    [self presentActivityController:controller];
+}
+
+- (IBAction)shareExcludeActions:(id)sender {
+    
+    // grab an item we want to share
+    UIImage *image = [UIImage imageNamed:@"three"];
+    NSArray *items = @[image];
+    
+    // build an activity view controller
+    UIActivityViewController *controller = [[UIActivityViewController alloc]initWithActivityItems:items applicationActivities:nil];
+    
+    // exclude several items (for example, Facepuke and Shitter)
+    NSArray *excluded = @[UIActivityTypePrint, UIActivityTypeSaveToCameraRoll, UIActivityTypeAirDrop, UIActivityTypeAddToReadingList];
+    controller.excludedActivityTypes = excluded;
+    
+    // and present it
+    [self presentActivityController:controller];
+}
+
+- (IBAction)shareText:(id)sender {
+    
+    // create a message
+    NSString *theMessage = @"Some text we're sharing with an activity controller";
+    NSArray *items = @[theMessage];
+    
+    // build an activity view controller
+    UIActivityViewController *controller = [[UIActivityViewController alloc]initWithActivityItems:items applicationActivities:nil];
+    
+    // and present it
+    [self presentActivityController:controller];
+}
+
+- (void)presentActivityController:(UIActivityViewController *)controller {
+    
+    // iPad: make the presentation a Popover
+    controller.modalPresentationStyle = UIModalPresentationPopover;
+    [self presentViewController:controller animated:YES completion:nil];
+    
+    UIPopoverPresentationController *popController = [controller popoverPresentationController];
+    popController.permittedArrowDirections = UIPopoverArrowDirectionAny;
+    popController.barButtonItem = self.navigationItem.leftBarButtonItem;
+    
+}
+
 
 @end
